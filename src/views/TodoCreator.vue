38571b311497b9e4aa3a831109a0a0e2c2c2f3d7
Noel Jose <noeljose@Noels-MacBook-Pro.local>
2023-11-07 16:36:44 +0530

    added new style for invalid state

--- src/views/TodoCreator.vue
+++ src/views/TodoCreator.vue
@@ -2,7 +2,11 @@
 <script setup>
     import { ref,reactive,defineEmits } from 'vue';
     const emit = defineEmits(['create-todo'])
-    const todo =  ref("")
+    const todoState = reactive({
+      todo:"",
+      invalid:null,
+      errMsg:""
+    })
     // to access value in ref in a script  we use todo.value
     // in the ref method we can only work with primitive data types
     // const todoState = reactive({
@@ -13,14 +17,24 @@
     // in the reactive method we can work with not only primitive data
 
     const createTodo = () => {
-        emit('create-todo',todo.value)
+      todoState.invalid = null
+      if(todoState.todo !== ""){
+        emit('create-todo',todoState.todo)
+        todoState.todo = ""
+        return;
+      }
+      todoState.invalid = true;
+      todoState.errMsg = "Todo value cannnot be empty"
     }
 </script>
 <template>
-    <div class="input-wrap">
-        <input type="text" name="" id="" v-model="todo">
+  <div>
+    <div class="input-wrap" :class="{'input-err':todoState.invalid}">
+        <input type="text" name="" id="" v-model="todoState.todo">
         <button @click="createTodo">Create</button>
     </div>
+    <p v-show="todoState.invalid" class="err-msg">{{todoState.errMsg}}</p>
+</div>
 </template>
 
 <style lang="scss" scoped>
@@ -28,6 +42,10 @@
   display: flex;
   transition: 250ms ease;
   border: 2px solid #41b080;
+
+  &.input-err {
+    border-color: red;
+  }
 
   &:focus-within {
     box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
@@ -48,5 +66,12 @@
     padding: 8px 16px;
     border: none;
   }
+}
+
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
 }
 </style>
